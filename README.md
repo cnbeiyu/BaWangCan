0bea3e4fca83d1eac41d51846a5014d9c4bba2b9
cnbeiyu <beiyu000@hotmail.com>
2019-11-18 21:35:47 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # BaWangCan
 大众点评一键报名所有霸王餐
+## 运行环境
+需要Python3.x的环境，没有的可以看[廖雪峰的Python安装教程](https://www.liaoxuefeng.com/wiki/1016959663602400/1016959856222624)。
+另外可能还需要几个三方库，可以直接用pip install安装。
+## 使用指南
+使用前首先需要自行在浏览器中获取cookie中的dper值，然后写入config.ini并保存。比如在chrome中随便找到一个霸王餐的报名链接，然后按下F12打开开发者工具，按下“报名”按钮之后再Network标签中找到一个cookies带有dper的请求就可以了。
+![chrome中的cookies获取](https://raw.githubusercontent.com/cnbeiyu/MarkdownPhotos/master/projectPhoto/BaWangCan/chrome%20cookie.png)
+config.ini中的cityId是指所需要获取霸王餐活动的所在城市，也是需要再浏览器中切换所在城市然后找到一个名为ajaxList的Post请求，请求中的cityId就是所需要城市的cityId,将cityId写入config.ini中即可。
+这里给出几个常见的大众点评cityId
+- 上海 1
+- 北京 2
+- 杭州 3
+- 广州 4
+- 南京 5
+- 苏州 6
+- 深圳 7
+- 成都 8
+- 重庆 9
+- 天津 10
+
+配置好config中的dper和cityId后就可以直接运行了，等待一段时间后会在当前目录生成名为霸王餐+当前时间的excel表格，表格中记录的霸王餐的报名情况。
+![运行结果](https://raw.githubusercontent.com/cnbeiyu/MarkdownPhotos/master/projectPhoto/BaWangCan/excel.png)
---
+++ bawangcan.py
@@ -0,0 +1,146 @@
+import requests
+import json
+import re
+import io
+import xlwt
+import datetime
+import configparser
+import os
+from tqdm import tqdm
+
+#从config.ini中读取dper和cityId
+cf = configparser.ConfigParser()
+path = os.path.abspath('config.ini')
+cf.read(path)
+dper = cf.get('basic', 'dper')
+cityId = cf.get('basic','cityId')
+
+# 创建一个workbook
+workbook = xlwt.Workbook(encoding = 'utf-8')
+# 创建一个worksheet
+worksheet = workbook.add_sheet('霸王餐')
+row0 = ["序号","霸王餐名称","报名状态","报名地址","异常原因"]
+for i in range(0,len(row0)):
+    worksheet.write(0,i,row0[i])
+
+orgin_url = 'http://s.dianping.com/event/'
+
+#获取霸王餐列表
+headers = {
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Referer': 'http://s.dianping.com/event/beijing',
+    'Origin': 'http://s.dianping.com',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36',
+    'Content-Type': 'application/json',
+}
+
+
+ids = []
+activityTitles = []
+data = {"cityId":cityId,"type":0,"mode":"","page":1}
+for page in range(1,12):
+    data["page"] = str(page)
+    response = requests.post('http://m.dianping.com/activity/static/pc/ajaxList', headers=headers, data=str(json.dumps(data)))
+    # print(page)
+    for item in response.json()['data']['detail']:
+        activityTitles.append(item['activityTitle'])
+        ids.append(item['offlineActivityId'])
+        print(str(page) + ":" + item['activityTitle'])
+
+print('搜索到'+str(len(ids))+'条霸王餐')
+cookies = {
+    'dper':dper
+}
+
+#报名霸王餐
+headers = {
+    'Origin': 'http://s.dianping.com',
+    'Accept-Encoding': 'gzip, deflate',
+    'X-Request': 'JSON',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8;',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+    'Accept': 'application/json, text/javascript',
+    'Referer': 'http://s.dianping.com/event/1063463422',
+    'X-Requested-With': 'XMLHttpRequest',
+    'Connection': 'keep-alive',
+}
+
+data = {
+  'offlineActivityId': '1063463422',
+  'phoneNo': '180****3715',
+  'shippingAddress': '',
+  'extraCount': '',
+  'birthdayStr': '',
+  'email': '',
+  'marryDayStr': '',
+  'babyBirths': '',
+  'pregnant': '',
+  'marryStatus': '0',
+  'comboId': '',
+  'branchId': '568675',
+  'usePassCard': '0',
+  'passCardNo': '',
+  'isShareSina': 'false',
+  'isShareQQ': 'false'
+}
+def set_color(color,bold):
+    style=xlwt.XFStyle()
+    font=xlwt.Font()
+    font.colour_index=color
+    font.bold = bold
+    style.font=font
+    return style
+
+success = []
+successed = []
+fail = []
+count = 1
+for _id in tqdm(ids):
+    text = requests.get(orgin_url+str(_id),headers=headers,cookies=cookies).text
+    shopid = re.search(r'shopid:[0-9]*',text).group()
+    shopid = shopid.split('shopid:')[1]
+    data['offlineActivityId'] = str(_id)
+    data['branchId'] = shopid
+    response = requests.post('http://s.dianping.com/ajax/json/activity/offline/saveApplyInfo', headers=headers,cookies=cookies, data=data)
+
+    msg = json.loads(response.text)
+
+    if "不要重复报名" in msg["msg"]:
+        successed.append(activityTitles[ids.index(_id)])
+        worksheet.write(count,0,count)
+        worksheet.write(count,1,activityTitles[ids.index(_id)])
+        worksheet.write(count,2,"已报名过")
+        worksheet.write(count,3,"http://s.dianping.com/event/"+str(_id))
+    elif "报名成功" in msg["msg"]:
+        success.append(activityTitles[ids.index(_id)])
+        worksheet.write(count,0,count)
+        worksheet.write(count,1,activityTitles[ids.index(_id)])
+        worksheet.write(count,2,"报名成功")
+        worksheet.write(count,3,"http://s.dianping.com/event/"+str(_id))
+    else :
+        print(msg)
+        fail.append(activityTitles[ids.index(_id)])
+        worksheet.write(count,0,count)
+        worksheet.write(count,1,activityTitles[ids.index(_id)])
+        worksheet.write(count,2,"报名异常",set_color(0x02,True))
+        worksheet.write(count,3,"http://s.dianping.com/event/"+str(_id))
+        worksheet.write(count,4,msg["msg"]["html"])
+    count=count+1
+worksheet.write(0,5,'成功登记'+str(len(success))+'个活动')
+worksheet.write(1,5,'已经登记过'+str(len(successed))+'个活动')
+worksheet.write(2,5,'登记异常'+str(len(fail))+'个活动')
+nowTime=datetime.datetime.now().strftime('%Y-%m-%d %H`%M`%S')#现在
+workbook.save('霸王餐'+nowTime+'.xls')
+
+print('成功登记'+str(len(success))+'个活动')
+# for i in success:
+#     print(i)
+
+print('已经登记过'+str(len(successed))+'个活动')
+# for i in successed:
+#     print(i)
+
+print('登记异常'+str(len(fail))+'个活动')
+# for i in fail:
+#     print(i)
